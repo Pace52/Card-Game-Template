3cd71154df382c79470d2aa151f0543243fa35fe
Yum-Num <ynakamura9447@student.pps.net>
2025-02-19 13:42:12 -0800

    i did so much work and pace did nothing cause hes useless and trash and bum ahh (Jk i love him) (tough love)

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -7,12 +7,10 @@ using UnityEngine.UI;
 public partial class Card : MonoBehaviour
 {
     public Card_data data;
-
-    public string card_name;
-    public string description;
-    public int health;
-    public int cost;
-    public int damage;
+    
+    public int value;
+    public string suit;
+    public int type;
     public Sprite sprite;
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI descriptionText;
@@ -25,17 +23,10 @@ public partial class Card : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        card_name = data.card_name;
-        description = data.description;
-        health = data.health;
-        cost = data.cost;
-        damage = data.damage;
+        value = data.value;
+        suit = data.suit;
+        type = data.type;
         sprite = data.sprite;
-        nameText.text = card_name;
-        descriptionText.text = description;
-        healthText.text = health.ToString();
-        costText.text = cost.ToString();
-        damageText.text = damage.ToString();
         spriteImage.sprite = sprite;
 
     }
