587103a0c323f35547e89408bb6471710d4b1237
Yum-Num <ynakamura9447@student.pps.net>
2025-02-27 13:25:04 -0800

    a
    a

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -11,6 +11,8 @@ public partial class Card : MonoBehaviour
     public int value;
     public string suit;
     public int type;
+    public string valueString; // "2" through "10", "J", "Q", "K", "A"
+    public string suitString; // Optional: "Hearts", "Diamonds", "Clubs", "Spades"
 
 
     // Start is called before the first frame update
@@ -21,7 +23,8 @@ public partial class Card : MonoBehaviour
         type = data.type;
         sprite = data.sprite;
         //spriteImage.sprite = sprite;
-
+        valueString = value.ToString();
+        suitString = suit;
     }
 
     // Update is called once per frame
